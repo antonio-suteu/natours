e7abb5f8df654b2c62db35c2d3a13b198903a39f
Antonio Suteu <suteu.antonio@gmail.com>
2025-03-29 22:23:12 +0100

    created viewController and viewRoutes

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-/// routers
+// routers
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -86,13 +87,7 @@ app.use((req, res, next) => {
 
 // 2) MOUNTING ROUTES
 // needed for server side rendering, each HTTP GET response gets converted into HTML
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Test User'
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/reviews', reviewRouter);
