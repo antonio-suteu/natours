5bc4ce887ffb8570e2b75da2941726d4bcea4290
Antonio Suteu <suteu.antonio@gmail.com>
2025-03-29 23:43:26 +0100

    Created tour overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,10 +1,20 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getTour = (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get all tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render that template using tour data from 1)
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours
+  });
+});
+
+exports.getTour = async (req, res, next) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker'
   });
